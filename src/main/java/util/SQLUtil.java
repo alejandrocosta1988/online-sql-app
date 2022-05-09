e8f632110511a36e55f34fabf829664e9697cd12
Alejandro Costa <alejandroesfc@gmail.com>
2022-05-09 06:39:47 -0300

    creates a utility class to build a html table with sql statement execution result

---
+++ src/main/java/util/SQLUtil.java
@@ -0,0 +1,43 @@
+package util;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+public class SQLUtil {
+
+	public static String getHtmlTable(ResultSet results) throws SQLException {
+		
+		StringBuilder htmlTable = new StringBuilder();
+		ResultSetMetaData metaData = results.getMetaData();
+		int columnCount = metaData.getColumnCount();
+		
+		htmlTable.append("<table>");
+		
+		htmlTable.append("<thead>");
+		for (int i = 0; i < columnCount; i++ ) {
+			htmlTable.append("<th>");
+			htmlTable.append(metaData.getColumnName(i));
+			htmlTable.append("</th>");
+		}
+		htmlTable.append("</thead>");
+
+		htmlTable.append("<tbody>");
+		while (results.next()) {
+			htmlTable.append("<tr>");
+			for (int i = 0; i < columnCount; i++) {
+				htmlTable.append("<td>");
+				htmlTable.append(results.getString(i));
+				htmlTable.append("</td>");
+			}
+			htmlTable.append("<tr>");
+		}
+		htmlTable.append("</tbody>");
+
+		htmlTable.append("</table>");
+		
+		return htmlTable.toString();
+		
+	}
+	
+}
